96f919555132b791987cc15a7c7a5767d06b29ee
Milica <djoric.milica91@yahoo.com>
2021-04-24 16:53:10 -0400

    added more line of code

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -3,6 +3,8 @@ package day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Adding GitHub Now");
+        System.out.println("Yay stffes are on gitHubu");
 
     }
 }
